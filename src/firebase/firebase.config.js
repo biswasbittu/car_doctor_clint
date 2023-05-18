af96294cc2b6789e0e042567c7e96cdea26bbd7f
biswasbittu <biswasbittu7415@yahoo.com>
2023-05-18 12:51:35 +0530

    sign up sign in done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -21,4 +21,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
-import router from './Routes/Routes';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import AuthProvider from "./provider/AuthProvider";
+import router from "./Routes/Routes";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
- <div className='container mx-auto'>
-   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
- </div>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="container mx-auto">
+    <React.StrictMode>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </React.StrictMode>
+    ,
+  </div>
+);
